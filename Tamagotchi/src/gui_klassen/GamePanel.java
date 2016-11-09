6934fe33540b88d78cd5655fc0b7fb6816c59004
Schmittma <schmittma@elektronikschule.de>
2016-11-10 00:22:56 +0100

    Images in der Gamepanel Klasse eingefügt und anzeigbar

--- Tamagotchi/src/gui_klassen/GamePanel.java
+++ Tamagotchi/src/gui_klassen/GamePanel.java
@@ -3,7 +3,11 @@ package gui_klassen;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import tamagotchi_klassen.Viech;
@@ -11,9 +15,24 @@ import tamagotchi_klassen.Viech;
 public class GamePanel extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
+	private Image hunger;
+	private Image durst;
 	
 	public GamePanel(Viech tamagotchi, Dimension d){
 		this.setPreferredSize(d);
+		hunger = null;
+		durst = null;
+		try{
+			hunger = ImageIO.read(new File("Hunger.png"));
+			durst = ImageIO.read(new File("Durst.png"));
+			
+			
+		}catch(NullPointerException e){
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		
 	}
 	
@@ -26,9 +45,21 @@ public class GamePanel extends JPanel {
 		g.drawLine(b, 1, b, h);
 		g.drawLine(1, h, b, h);
 		
-			
-		printBeduerfnis(g, 20, 20, Viech.hunger.getWert());
-		printBeduerfnis(g, 20, 10, Viech.durst.getWert());
+		int hungerXpara = 20;
+		int hungerYpara = 20;
+		
+		int durstXpara = 20;
+		int durstYpara = 10;
+		
+		
+		if(!(hunger == null) & !(durst == null)){
+			g.drawImage(hunger, this.getWidth()/hungerXpara - 25, this.getHeight()/hungerYpara, this);
+			g.drawImage(durst, this.getWidth()/durstXpara - 25, this.getHeight()/durstYpara, this);
+		}
+		
+
+		printBeduerfnis(g, hungerXpara, hungerYpara, Viech.hunger.getWert());
+		printBeduerfnis(g, durstXpara, durstYpara, Viech.durst.getWert());
 		
 		
 //		String beduerfnisse = "" + this.tamagotchi;
@@ -62,4 +93,6 @@ public class GamePanel extends JPanel {
 		g.drawRect(startX, startY, b, h);		
 	}
 	
+	 
+
 }
